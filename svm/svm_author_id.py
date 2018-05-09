168b18926e4cf920526cf82e085f4b0c76aa8de4
Bahaareh <bn_1377@yahoo.com>
2018-05-09 18:17:00 +0430

    [SVM] Extracting Predictions from an SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,7 +7,7 @@
     Sara has label 0
     Chris has label 1
 """
-
+ 
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -20,9 +20,9 @@ from sklearn.metrics import accuracy_score
 # labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-# features_train = features_train[:int(len(features_train)/100)]
-# labels_train = labels_train[:int(len(labels_train)/100)]
-clf = SVC(C=1000, kernel='rbf')
+features_train = features_train[:int(len(features_train)/100)]
+labels_train = labels_train[:int(len(labels_train)/100)]
+clf = SVC(C=10000, kernel='rbf')
 
 t1 = time()
 clf.fit(features_train, labels_train)
@@ -34,3 +34,7 @@ print("test time:", round(time()-t2, 3), "s")
 
 acc = accuracy_score(labels_test, pred)
 print(acc)
+
+print(pred[10])
+print(pred[26])
+print(pred[50])
